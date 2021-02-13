c1bcb54014e217fcd6a7267ac978fa2a57504ddb
kevin dorado <kadoradob@gmail.com>
2021-02-13 08:35:50 -0500

    update url of 22 challenges

--- app.js
+++ app.js
@@ -33,29 +33,118 @@ const challenges = [
     name: "Rotating Navigation",
     url: "https://reto3-ten.vercel.app/",
   },
-  { img: "./img/challenge4.png", name: "Hidden Search", url: "" },
-  { img: "./img/challenge5.png", name: "Blurry Loading", url: "" },
-  { img: "./img/challenge6.png", name: "Drag N Drop", url: "" },
-  { img: "./img/challenge7.png", name: "AutoText", url: "" },
-  { img: "./img/challenge8.png", name: "HoverBoard", url: "" },
-  { img: "./img/challenge9.png", name: "Scroll Animation", url: "" },
-  { img: "./img/challenge10.png", name: "Form input wave", url: "" },
-  { img: "./img/challenge11.png", name: "Dad Jokes", url: "" },
-  { img: "./img/challenge12.png", name: "Sound Board", url: "" },
-  { img: "./img/challenge13.png", name: "Live User Filter", url: "" },
-  { img: "./img/challenge14.png", name: "Button Ripple Effect", url: "" },
-  { img: "./img/challenge15.png", name: "Background Slider", url: "" },
-  { img: "./img/challenge16.png", name: "Kinetic Loader", url: "" },
-  { img: "./img/challenge17.png", name: "Netflix Navigation", url: "" },
-  { img: "./img/challenge18.png", name: "Testimonial Box Switcher", url: "" },
-  { img: "./img/challenge19.png", name: "Pokedex", url: "" },
-  { img: "./img/challenge20.png", name: "Doble Click Heart", url: "" },
-  { img: "./img/challenge21.png", name: "Event Keycodes", url: "" },
-  { img: "./img/challenge22.png", name: "Mobile Nav Navigation", url: "" },
-  { img: "./img/challenge23.png", name: "Drink Water", url: "" },
-  { img: "./img/challenge24.png", name: "Feedback UI Design", url: "" },
-  { img: "./img/challenge25.png", name: "Github Profile", url: "" },
+  {
+    img: "./img/challenge4.png",
+    name: "Hidden Search",
+    url: "https://reto4.vercel.app/",
+  },
+  {
+    img: "./img/challenge5.png",
+    name: "Blurry Loading",
+    url: "https://reto5.vercel.app/",
+  },
+  {
+    img: "./img/challenge6.png",
+    name: "Drag N Drop",
+    url: "https://reto6.vercel.app/",
+  },
+  {
+    img: "./img/challenge7.png",
+    name: "AutoText",
+    url: "https://reto7.vercel.app/",
+  },
+  {
+    img: "./img/challenge8.png",
+    name: "HoverBoard",
+    url: "https://reto8.vercel.app/",
+  },
+  {
+    img: "./img/challenge9.png",
+    name: "Scroll Animation",
+    url: "https://reto9.vercel.app/",
+  },
+  {
+    img: "./img/challenge10.png",
+    name: "Form input wave",
+    url: "https://reto10.vercel.app/",
+  },
+  {
+    img: "./img/challenge11.png",
+    name: "Dad Jokes",
+    url: "https://reto11.vercel.app/",
+  },
+  {
+    img: "./img/challenge12.png",
+    name: "Sound Board",
+    url: "https://vercel.com/kadorado/reto12",
+  },
+  {
+    img: "./img/challenge13.png",
+    name: "Live User Filter",
+    url: "https://vercel.com/kadorado/reto13",
+  },
+  {
+    img: "./img/challenge14.png",
+    name: "Button Ripple Effect",
+    url: "https://vercel.com/kadorado/reto14",
+  },
+  {
+    img: "./img/challenge15.png",
+    name: "Background Slider",
+    url: "https://vercel.com/kadorado/reto15",
+  },
+  {
+    img: "./img/challenge16.png",
+    name: "Kinetic Loader",
+    url: "https://reto16-ll90bzdzu.vercel.app/",
+  },
+  {
+    img: "./img/challenge17.png",
+    name: "Netflix Navigation",
+    url: "https://reto17-qvplwlj82.vercel.app/",
+  },
+  {
+    img: "./img/challenge18.png",
+    name: "Testimonial Box Switcher",
+    url: "https://reto18.vercel.app/",
+  },
+  {
+    img: "./img/challenge19.png",
+    name: "Pokedex",
+    url: "https://reto19.vercel.app/",
+  },
+  {
+    img: "./img/challenge20.png",
+    name: "Doble Click Heart",
+    url: "https://reto20.vercel.app/",
+  },
+  {
+    img: "./img/challenge21.png",
+    name: "Event Keycodes",
+    url: "https://reto21-puce.vercel.app/",
+  },
+  {
+    img: "./img/challenge22.png",
+    name: "Mobile Nav Navigation",
+    url: "https://reto22.vercel.app/",
+  },
+  {
+    img: "./img/challenge23.png",
+    name: "Drink Water",
+    url: "https://reto23.vercel.app/",
+  },
+  {
+    img: "./img/challenge24.png",
+    name: "Feedback UI Design",
+    url: "https://reto24.vercel.app/",
+  },
+  {
+    img: "./img/challenge25.png",
+    name: "Github Profile",
+    url: "https://reto25.vercel.app/",
+  },
 ];
+
 
 function writeTitle() {
   if (idx > title.length) {
